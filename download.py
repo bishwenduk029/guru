9908b28b7dc66e25f8fa62741bc3b1052716a2a4
Kundu, Bishwendu <Bishwendu.Kundu@in.pega.com>
2022-10-23 18:20:25 +0530

    Remove default revision

--- download.py
+++ download.py
@@ -6,17 +6,19 @@
 from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast
 import torch
 
+
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
     GPTNeoXForCausalLM.from_pretrained(
-        "EleutherAI/gpt-neox-20b", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True
-    )
+        "EleutherAI/gpt-neox-20b"
+    ).half().cuda()
     print("done")
 
     print("downloading tokenizer...")
     GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
     print("done")
+
 
 if __name__ == "__main__":
     download_model()
